d5e5360edd58b0178681d363c63843c9fef61cc3
yaxley-peaks <yax@yaxley.in>
2026-03-25 15:36:05 +0530

    initial commit

---
+++ src/test/java/in/yaxley/guestbook/GuestBookApplicationTests.java
@@ -0,0 +1,13 @@
+package in.yaxley.guestbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuestBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
