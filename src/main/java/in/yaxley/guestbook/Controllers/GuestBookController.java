a7c2c02df575bbbc346131fe495fc21d26215edd
yaxley-peaks <yax@yaxley.in>
2026-03-26 22:01:27 +0530

    i am a teapot

--- src/main/java/in/yaxley/guestbook/Controllers/GuestBookController.java
+++ src/main/java/in/yaxley/guestbook/Controllers/GuestBookController.java
@@ -4,22 +4,26 @@ import in.yaxley.guestbook.Models.GuestBookEntry;
 import in.yaxley.guestbook.Repositories.GuestBookEntryRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Calendar;
 import java.util.List;
 
+import static org.springframework.http.HttpStatus.I_AM_A_TEAPOT;
+
+
 @RestController
 public class GuestBookController {
-
     GuestBookEntryRepository guestBookEntryRepository;
 
     @Autowired
     public GuestBookController(GuestBookEntryRepository guestBookEntryRepository) {
         this.guestBookEntryRepository = guestBookEntryRepository;
+    }
+
+    @GetMapping("/coffee")
+    public void getCoffee() throws TeapotException {
+        throw new TeapotException();
     }
 
     @GetMapping("/")
@@ -38,8 +42,23 @@ public class GuestBookController {
         return new GuestBookEntry();
     }
 
-     static public class PostDto {
+    public static class TeapotException extends Exception {
+
+    }
+
+    static public class PostDto {
         public String submitter;
         public String message;
+    }
+}
+
+
+@ControllerAdvice
+class TeapotExceptionAdvice {
+    @SuppressWarnings("deprecation")
+    @ExceptionHandler(GuestBookController.TeapotException.class)
+    @ResponseStatus(I_AM_A_TEAPOT)
+    public void iAmATeapot() {
+        // do nothing
     }
 }
