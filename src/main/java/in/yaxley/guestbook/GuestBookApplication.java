a68e62c6225b4ed7010c5414a5c4732f42007c31
yaxley-peaks <yax@yaxley.in>
2026-03-27 21:38:16 +0530

    remove warnings

--- src/main/java/in/yaxley/guestbook/GuestBookApplication.java
+++ src/main/java/in/yaxley/guestbook/GuestBookApplication.java
@@ -1,21 +1,12 @@
 package in.yaxley.guestbook;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GuestBookApplication {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(GuestBookApplication.class, args);
-    }
-
-    @Bean
-    public CommandLineRunner helloRunner() {
-        return _ -> {
-            System.out.println("Hello, world!");
-        };
     }
 }
